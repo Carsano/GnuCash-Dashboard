085b6514469e28184bdded61f7868303b92f1c6c
Carsano <damien.fabiano@protonmail.com>
2025-12-13 23:45:47 +0100

    Add first streamlit interface

--- src/application/use_cases/__init__.py
+++ src/application/use_cases/__init__.py
@@ -1,6 +1,11 @@
 """Application use cases package."""
 
 from .sync_accounts import SyncAccountsUseCase, SyncAccountsResult
+from .get_accounts import GetAccountsUseCase, AccountDTO
 
-__all__ = ["SyncAccountsUseCase", "SyncAccountsResult"]
-
+__all__ = [
+    "SyncAccountsUseCase",
+    "SyncAccountsResult",
+    "GetAccountsUseCase",
+    "AccountDTO",
+]
